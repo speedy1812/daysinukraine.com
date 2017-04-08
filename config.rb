1f3f24dd6e1b00b7c31530b323432c8e346d3e45
Nathan Day <adayinukraine@gmail.com>
2017-04-08 21:16:49 +0300

    Add name of S3 bucket to config.rb

--- config.rb
+++ config.rb
@@ -74,7 +74,7 @@ end
 
 # https://github.com/fredjean/middleman-s3_sync
 activate :s3_sync do |s3|
-	s3.bucket = "gulp-mm-css"
+	s3.bucket = "daysinukraine.com"
 	s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
 	s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
 	s3.prefer_gzip = true
