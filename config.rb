82f67c7e288ade139b375bb3d22064d09be74d52
Nathan Day <adayinukraine@gmail.com>
2017-04-08 17:26:29 +0300

    Add directory indexes and 404 page

--- config.rb
+++ config.rb
@@ -16,9 +16,8 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
+activate :directory_indexes
+page "/404.html", directory_index: false
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
