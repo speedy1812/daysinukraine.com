760d203fcaa84606072fdc656a0174c0353a872e
Nathan Day <adayinukraine@gmail.com>
2017-04-21 12:11:05 +0300

    Change default post permalink to year/month/title

--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{title}.html"
   # Matcher for blog source files
   blog.sources = "blog/{year}-{month}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
