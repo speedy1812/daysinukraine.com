13f3fc2b33fd287b05521e63ed6c50efa5c8b569
Nathan Day <adayinukraine@gmail.com>
2017-05-31 17:14:54 +0300

    Implement asset hashing and pull assets straight from Cloudfront

--- environments/production.rb
+++ environments/production.rb
@@ -2,3 +2,10 @@
 # environment are stored in the build configuation block in config.rb.
 #
 # https://middlemanapp.com/basics/upgrade-v4/#environments-and-changes-to-configure-blocks
+
+activate :asset_host, host: "https://d1uwlk5da5dexa.cloudfront.net"
+activate :asset_hash
+
+# https://github.com/fredjean/middleman-s3_sync#http-caching
+default_caching_policy max_age: (60 * 60 * 24 * 365)
+caching_policy "text/html", public: true, max_age: 0, must_revalidate: true
