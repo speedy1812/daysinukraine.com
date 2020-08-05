93794e5db25e9bc2919314fae2732ba35d225e99
Nathan Day <adayinukraine@gmail.com>
2020-08-05 16:41:03 +0300

    Add Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './source/**/*.erb',
+    './source/**/*.scss',
+    './source/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
