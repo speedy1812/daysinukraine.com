1996ada182dd74cd58f250a2d3b76386c74a5f90
Nathan Day <adayinukraine@gmail.com>
2020-08-19 17:25:43 +0300

    Add non-sliding sidebar for mobile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
-console.info("Looks like Middleman + Gulp asset pipeline is working!");
+console.info("Looks like the Middleman + Gulp asset pipeline is working!");
+
+window.onload=function(){
+  const menu = document.querySelector('.menu');
+  const x = document.querySelector('.x');
+  const sidebar = document.querySelector('.sidebar');
+
+  menu.addEventListener('click', () => {
+    if (sidebar.classList.contains('hidden')) {
+      sidebar.classList.remove('hidden');
+    }
+  });
+
+  x.addEventListener('click', () => {
+    sidebar.classList.add('hidden');
+  });
+}
